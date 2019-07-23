6eea86ee11b0f9e2b4d2d049cc6d65c0fea767fc
Romany Sefen <u1662160@unimail.hud.ac.uk>
2019-07-24 01:35:16 +0300

    fixed the message when connected to DB

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -7,7 +7,7 @@ const connectDB = async () => {
   try {
     await mongoose.connect(db, { useNewUrlParser: true, useCreateIndex: true });
     console.log(
-      "Welcome Romany :: Your Monog DB is connected using mongoos .... "
+      "Welcome Romany :: Your Mongoo  DB is connected using mongoos .... "
     );
   } catch (e) {
     console.log("Error : " + e);
