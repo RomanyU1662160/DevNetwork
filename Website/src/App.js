aa93c9ceb7b24a39a178b9ebfa4b30941813d502
Romany Sefen <u1662160@unimail.hud.ac.uk>
2019-08-26 11:52:48 +0100

    creat new react appl developed the dev script

---
+++ Website/src/App.js
@@ -0,0 +1,12 @@
+import React, { Fragment } from "react";
+
+import "./App.css";
+
+const App = () => (
+  <Fragment>
+    <div className='App'>
+      <h1 className='text-info  text-center'> Welcome to DevNetwork</h1>
+    </div>
+  </Fragment>
+);
+export default App;
